d0432bf00f77707a5e8a20c61b228975bcf41979
Haydon Houghton <haydonhoughton@outlook.com>
2020-03-24 11:35:13 +0000

    Add deposit method to account

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,16 @@
 class Account
-  
-  def initialize
-    @balance = 0
+
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def get_balance
     return @balance
+  end
+  
+  def deposit(amount)
+    raise "Cannot deposit negative amounts" if amount.negative?
+    
+    @balance += amount
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,27 @@
 require 'account'
 
 describe Account do
-  context '#balance' do
+  context '#get_balance' do
     it 'should return the default balance of 0 when asked' do
       account = Account.new
       expect(account.get_balance).to eq(0)
+    end
+    
+    it "should return the amount the account was initialized with" do
+      account = Account.new(100)
+      expect(account.get_balance).to eq(100)
+    end
+  end
+  context '#deposit' do
+    it 'should add the deposited amount to the account balance' do
+      account = Account.new
+      account.deposit(1000)
+      expect(account.get_balance).to eq(1000)
+    end
+
+    it 'should not accept a negative number' do
+      account = Account.new
+      expect{ account.deposit(-1000) }.to raise_error("Cannot deposit negative amounts")
     end
   end
 end
