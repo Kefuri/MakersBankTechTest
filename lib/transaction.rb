dd4e176c0f9097541eab5a7c74dc5067bcee0846
Haydon Houghton <haydonhoughton@outlook.com>
2020-03-25 16:02:04 +0000

    Add get time method and testing to transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,19 @@
 class Transaction
-  def initialize(transaction_type)
+  def initialize(transaction_type, amount)
     @transaction_type = transaction_type
+    @amount = amount
+    @date = Time.new
   end
-  
+
   def get_type
     return @transaction_type
+  end
+
+  def get_amount
+    return @amount
+  end
+  
+  def get_date
+    return @date.strftime("%d/%m/%Y")
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,13 +1,36 @@
 require 'transaction'
 
 describe Transaction do
-  it "should return deposit as the transaction type" do
-    deposit = Transaction.new("Deposit")
-    expect(deposit.get_type).to eq("Deposit")
+
+  context "#get_type" do
+    it "should return deposit as the transaction type" do
+      deposit = Transaction.new("Deposit", 100)
+      expect(deposit.get_type).to eq("Deposit")
+    end
+
+    it "should return withdraw as the transaction type" do
+      withdraw = Transaction.new("Withdrawal", 100)
+      expect(withdraw.get_type).to eq("Withdrawal")
+    end
+  end
+  
+  context "#get_amount" do
+    it "should return the deposit amount when initialized" do
+      deposit = Transaction.new("Deposit", 300)
+      expect(deposit.get_amount).to eq(300)
+    end
   end
 
-  it "should return withdraw as the transaction type" do
-    withdraw = Transaction.new("Withdrawal")
-    expect(withdraw.get_type).to eq("Withdrawal")
+  before do
+    @fake_time = Time.new
+    allow(Time).to receive(:new).and_return(@fake_time)
+  end
+
+  context "#get_date" do
+    it "Should return the date that the account was made on" do
+      expect(Time).to receive(:new)
+      deposit = Transaction.new("Deposit", 300)
+      expect(deposit.get_date).to eq(Time.new.strftime("%d/%m/%Y"))
+    end
   end
 end
